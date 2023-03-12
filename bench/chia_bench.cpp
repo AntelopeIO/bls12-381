df0205a253c48e0c8c2f8d3925227d0d1a249153
mschoenebeck <matthias.schoenebeck@gmail.com>
2023-03-12 14:48:17 -0600

    reversed prev git mistake

---
+++ bench/chia_bench.cpp
@@ -0,0 +1,184 @@
+#include <chrono>
+#include <bls12_381.hpp>
+#include <iostream>
+#include <random>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+
+using namespace bls12_381;
+
+std::chrono::time_point<std::chrono::steady_clock> startStopwatch() {
+    return std::chrono::steady_clock::now();
+}
+
+void endStopwatch(string testName,
+                  std::chrono::time_point<std::chrono::steady_clock> start,
+                  int numIters) {
+    auto end = std::chrono::steady_clock::now();
+    auto now_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+            end - start);
+
+    cout << endl << testName << endl;
+    cout << "Total: " << numIters << " runs in " << now_ms.count()
+         << " ms" << endl;
+    cout << "Avg: " << now_ms.count() / static_cast<double>(numIters)
+         << " ms" << endl;
+}
+
+std::vector<uint8_t> getRandomSeed() {
+    uint64_t buf[4];
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_int_distribution<uint64_t> dis;
+    buf[0] = dis(gen);
+    buf[1] = dis(gen);
+    buf[2] = dis(gen);
+    buf[3] = dis(gen);
+    vector<uint8_t> ret(buf, buf + 32);
+    return ret;
+}
+
+void IntToFourBytes(uint8_t* result,
+                    const uint32_t input) {
+    for (size_t i = 0; i < 4; i++) {
+        result[3 - i] = (input >> (i * 8));
+    }
+}
+
+void benchSigs() {
+    string testName = "Signing";
+    const int numIters = 5000;
+    array<uint64_t, 4> sk = secret_key(getRandomSeed());
+    array<uint8_t, 48UL> pk = public_key(sk).toCompressedBytesBE();
+    vector<uint8_t> message1(pk.begin(), pk.end());
+
+    auto start = startStopwatch();
+
+    for (int i = 0; i < numIters; i++) {
+        sign(sk, message1);
+    }
+    endStopwatch(testName, start, numIters);
+}
+
+void benchVerification() {
+    string testName = "Verification";
+    const int numIters = 10000;
+    array<uint64_t, 4> sk = secret_key(getRandomSeed());
+    g1 pk = public_key(sk);
+
+    std::vector<g2> sigs;
+
+    for (int i = 0; i < numIters; i++) {
+        uint8_t message[4];
+        IntToFourBytes(message, i);
+        vector<uint8_t> messageBytes(message, message + 4);
+        sigs.push_back(sign(sk, messageBytes));
+    }
+
+    auto start = startStopwatch();
+    for (int i = 0; i < numIters; i++) {
+        uint8_t message[4];
+        IntToFourBytes(message, i);
+        vector<uint8_t> messageBytes(message, message + 4);
+        bool ok = verify(pk, messageBytes, sigs[i]);
+        if(!ok) throw invalid_argument("benchVerification: !ok");
+    }
+    endStopwatch(testName, start, numIters);
+}
+
+void benchBatchVerification() {
+    const int numIters = 100000;
+
+    vector<array<uint8_t, 96UL>> sig_bytes;
+    vector<array<uint8_t, 48UL>> pk_bytes;
+    vector<vector<uint8_t>> ms;
+
+    for (int i = 0; i < numIters; i++) {
+        uint8_t message[4];
+        IntToFourBytes(message, i);
+        vector<uint8_t> messageBytes(message, message + 4);
+        array<uint64_t, 4> sk = secret_key(getRandomSeed());
+        g1 pk = public_key(sk);
+        sig_bytes.push_back(sign(sk, messageBytes).toCompressedBytesBE());
+        pk_bytes.push_back(pk.toCompressedBytesBE());
+        ms.push_back(messageBytes);
+    }
+
+    vector<g1> pks;
+    pks.reserve(numIters);
+
+    auto start = startStopwatch();
+    for (auto const& pk : pk_bytes) {
+        pks.emplace_back(g1::fromCompressedBytesBE(pk));
+    }
+    endStopwatch("Public key validation", start, numIters);
+
+    vector<g2> sigs;
+    sigs.reserve(numIters);
+
+    start = startStopwatch();
+    for (auto const& sig : sig_bytes) {
+        sigs.emplace_back(g2::fromCompressedBytesBE(sig));
+    }
+    endStopwatch("Signature validation", start, numIters);
+
+    start = startStopwatch();
+    g2 aggSig = aggregate_signatures(sigs);
+    endStopwatch("Aggregation", start, numIters);
+
+    start = startStopwatch();
+    bool ok = aggregate_verify(pks, ms, aggSig);
+    if(!ok) throw invalid_argument("benchBatchVerification: !ok");
+    endStopwatch("Batch verification", start, numIters);
+}
+
+void benchFastAggregateVerification() {
+    const int numIters = 5000;
+
+    vector<g2> sigs;
+    vector<g1> pks;
+    vector<uint8_t> message = {1, 2, 3, 4, 5, 6, 7, 8};
+    vector<g2> pops;
+
+    for (int i = 0; i < numIters; i++) {
+        array<uint64_t, 4> sk = secret_key(getRandomSeed());
+        g1 pk = public_key(sk);
+        sigs.push_back(sign(sk, message));
+        pops.push_back(pop_prove(sk));
+        pks.push_back(pk);
+    }
+
+    auto start = startStopwatch();
+    g2 aggSig = aggregate_signatures(sigs);
+    endStopwatch("PopScheme Aggregation", start, numIters);
+
+
+    start = startStopwatch();
+    for (int i = 0; i < numIters; i++) {
+        bool ok = pop_verify(pks[i], pops[i]);
+        if(!ok) throw invalid_argument("benchBatchVerification, pop_verify: !ok");
+    }
+    endStopwatch("PopScheme Proofs verification", start, numIters);
+
+    start = startStopwatch();
+    bool ok = pop_fast_aggregate_verify(pks, message, aggSig);
+    if(!ok) throw invalid_argument("benchBatchVerification, pop_fast_aggregate_verify: !ok");
+    endStopwatch("PopScheme verification", start, numIters);
+}
+
+int main(int argc, char* argv[])
+{
+    benchSigs();
+    benchVerification();
+    benchBatchVerification();
+    benchFastAggregateVerification();
+}
--- build/bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mschoenebeck/dev/bls12_381/bench
-# Build directory: /home/mschoenebeck/dev/bls12_381/build/bench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
