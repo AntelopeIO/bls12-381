2caa6354dbf3f97ac67c0965aa21effa346cd699
mschoenebeck <matthias.schoenebeck@gmail.com>
2023-03-15 18:37:02 -0600

    added more asm functions to improve performance

--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -91,6 +91,67 @@ void _add(fp* z, const fp* x, const fp* y)
 }
 #endif
 
+#ifdef __x86_64__
+void _addAssign(fp* x, const fp* y)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // x => %rdi
+    // y => %rsi
+    // callee needs to restore registers r15, r14, r13, r12, rbx before returning
+    asm("push %r15;");
+    asm("push %r14;");
+    asm("push %r13;");
+    asm("push %r12;");
+    asm("push %rbx;");
+    asm("mov     (%rdi),%r8;");
+    asm("mov 0x08(%rdi),%r9;");
+    asm("mov 0x10(%rdi),%r10;");
+    asm("mov 0x18(%rdi),%r11;");
+    asm("mov 0x20(%rdi),%r12;");
+    asm("mov 0x28(%rdi),%r13;");
+    asm("add     (%rsi),%r8;");
+    asm("adc 0x08(%rsi),%r9;");
+    asm("adc 0x10(%rsi),%r10;");
+    asm("adc 0x18(%rsi),%r11;");
+    asm("adc 0x20(%rsi),%r12;");
+    asm("adc 0x28(%rsi),%r13;");
+    asm("mov %r8,%r14;");
+    asm("mov %r9,%r15;");
+    asm("mov %r10,%rcx;");
+    asm("mov %r11,%rdx;");
+    asm("mov %r12,%rsi;");
+    asm("mov %r13,%rbx;");
+    asm("mov $0xb9feffffffffaaab,%rax;");
+    asm("sub %rax,%r14;");
+    asm("mov $0x1eabfffeb153ffff,%rax;");
+    asm("sbb %rax,%r15;");
+    asm("mov $0x6730d2a0f6b0f624,%rax;");
+    asm("sbb %rax,%rcx;");
+    asm("mov $0x64774b84f38512bf,%rax;");
+    asm("sbb %rax,%rdx;");
+    asm("mov $0x4b1ba7b6434bacd7,%rax;");
+    asm("sbb %rax,%rsi;");
+    asm("mov $0x1a0111ea397fe69a,%rax;");
+    asm("sbb %rax,%rbx;");
+    asm("cmovae %r14,%r8;");
+    asm("cmovae %r15,%r9;");
+    asm("cmovae %rcx,%r10;");
+    asm("cmovae %rdx,%r11;");
+    asm("cmovae %rsi,%r12;");
+    asm("cmovae %rbx,%r13;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %r12,0x20(%rdi);");
+    asm("mov %r13,0x28(%rdi);");
+    asm("pop %rbx;");
+    asm("pop %r12;");
+    asm("pop %r13;");
+    asm("pop %r14;");
+    asm("pop %r15;");
+}
+#else
 void _addAssign(fp* x, const fp* y)
 {
     uint64_t carry, _;
@@ -115,7 +176,36 @@ void _addAssign(fp* x, const fp* y)
         tie(x->d[5], _) = Sub64(x->d[5], fp::MODULUS.d[5], b);
     }
 }
+#endif
 
+#ifdef __x86_64__
+void _ladd(fp* z, const fp* x, const fp* y)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // x => %rsi
+    // y => %rdx
+    // callee needs to restore registers r15, r14, r13, r12, rbx (if clobbed) before returning
+    asm("mov     (%rsi),%r8;");
+    asm("mov 0x08(%rsi),%r9;");
+    asm("mov 0x10(%rsi),%r10;");
+    asm("mov 0x18(%rsi),%r11;");
+    asm("mov 0x20(%rsi),%rcx;");
+    asm("mov 0x28(%rsi),%rax;");
+    asm("add     (%rdx),%r8;");
+    asm("adc 0x08(%rdx),%r9;");
+    asm("adc 0x10(%rdx),%r10;");
+    asm("adc 0x18(%rdx),%r11;");
+    asm("adc 0x20(%rdx),%rcx;");
+    asm("adc 0x28(%rdx),%rax;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %rcx,0x20(%rdi);");
+    asm("mov %rax,0x28(%rdi);");
+}
+#else
 void _ladd(fp* z, const fp* x, const fp* y)
 {
     uint64_t carry, _;
@@ -126,7 +216,35 @@ void _ladd(fp* z, const fp* x, const fp* y)
     tie(z->d[4], carry) = Add64(x->d[4], y->d[4], carry);
     tie(z->d[5], _)     = Add64(x->d[5], y->d[5], carry);
 }
+#endif
 
+#ifdef __x86_64__
+void _laddAssign(fp* x, const fp* y)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // x => %rdi
+    // y => %rsi
+    // callee needs to restore registers r15, r14, r13, r12, rbx (if clobbed) before returning
+    asm("mov     (%rdi),%r8;");
+    asm("mov 0x08(%rdi),%r9;");
+    asm("mov 0x10(%rdi),%r10;");
+    asm("mov 0x18(%rdi),%r11;");
+    asm("mov 0x20(%rdi),%rcx;");
+    asm("mov 0x28(%rdi),%rax;");
+    asm("add     (%rsi),%r8;");
+    asm("adc 0x08(%rsi),%r9;");
+    asm("adc 0x10(%rsi),%r10;");
+    asm("adc 0x18(%rsi),%r11;");
+    asm("adc 0x20(%rsi),%rcx;");
+    asm("adc 0x28(%rsi),%rax;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %rcx,0x20(%rdi);");
+    asm("mov %rax,0x28(%rdi);");
+}
+#else
 void _laddAssign(fp* x, const fp* y)
 {
     uint64_t carry, _;
@@ -137,7 +255,69 @@ void _laddAssign(fp* x, const fp* y)
     tie(x->d[4], carry) = Add64(x->d[4], y->d[4], carry);
     tie(x->d[5], _)     = Add64(x->d[5], y->d[5], carry);
 }
+#endif
 
+#ifdef __x86_64__
+void _double(fp* z, const fp* x)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // x => %rsi
+    // callee needs to restore registers r15, r14, r13, r12, rbx before returning
+    asm("push %r15;");
+    asm("push %r14;");
+    asm("push %r13;");
+    asm("push %r12;");
+    asm("push %rbx;");
+    asm("mov     (%rsi),%r8;");
+    asm("mov 0x08(%rsi),%r9;");
+    asm("mov 0x10(%rsi),%r10;");
+    asm("mov 0x18(%rsi),%r11;");
+    asm("mov 0x20(%rsi),%r12;");
+    asm("mov 0x28(%rsi),%r13;");
+    asm("add %r8,%r8;");
+    asm("adc %r9,%r9;");
+    asm("adc %r10,%r10;");
+    asm("adc %r11,%r11;");
+    asm("adc %r12,%r12;");
+    asm("adc %r13,%r13;");
+    asm("mov %r8,%r14;");
+    asm("mov %r9,%r15;");
+    asm("mov %r10,%rcx;");
+    asm("mov %r11,%rdx;");
+    asm("mov %r12,%rsi;");
+    asm("mov %r13,%rbx;");
+    asm("mov $0xb9feffffffffaaab,%rax;");
+    asm("sub %rax,%r14;");
+    asm("mov $0x1eabfffeb153ffff,%rax;");
+    asm("sbb %rax,%r15;");
+    asm("mov $0x6730d2a0f6b0f624,%rax;");
+    asm("sbb %rax,%rcx;");
+    asm("mov $0x64774b84f38512bf,%rax;");
+    asm("sbb %rax,%rdx;");
+    asm("mov $0x4b1ba7b6434bacd7,%rax;");
+    asm("sbb %rax,%rsi;");
+    asm("mov $0x1a0111ea397fe69a,%rax;");
+    asm("sbb %rax,%rbx;");
+    asm("cmovae %r14,%r8;");
+    asm("cmovae %r15,%r9;");
+    asm("cmovae %rcx,%r10;");
+    asm("cmovae %rdx,%r11;");
+    asm("cmovae %rsi,%r12;");
+    asm("cmovae %rbx,%r13;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %r12,0x20(%rdi);");
+    asm("mov %r13,0x28(%rdi);");
+    asm("pop %rbx;");
+    asm("pop %r12;");
+    asm("pop %r13;");
+    asm("pop %r14;");
+    asm("pop %r15;");
+}
+#else
 void _double(fp* z, const fp* x)
 {
     uint64_t carry, _;
@@ -162,7 +342,68 @@ void _double(fp* z, const fp* x)
         tie(z->d[5], _) = Sub64(z->d[5], fp::MODULUS.d[5], b);
     }
 }
+#endif
 
+#ifdef __x86_64__
+void _doubleAssign(fp* z)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // callee needs to restore registers r15, r14, r13, r12, rbx before returning
+    asm("push %r15;");
+    asm("push %r14;");
+    asm("push %r13;");
+    asm("push %r12;");
+    asm("push %rbx;");
+    asm("mov     (%rdi),%r8;");
+    asm("mov 0x08(%rdi),%r9;");
+    asm("mov 0x10(%rdi),%r10;");
+    asm("mov 0x18(%rdi),%r11;");
+    asm("mov 0x20(%rdi),%r12;");
+    asm("mov 0x28(%rdi),%r13;");
+    asm("add %r8,%r8;");
+    asm("adc %r9,%r9;");
+    asm("adc %r10,%r10;");
+    asm("adc %r11,%r11;");
+    asm("adc %r12,%r12;");
+    asm("adc %r13,%r13;");
+    asm("mov %r8,%r14;");
+    asm("mov %r9,%r15;");
+    asm("mov %r10,%rcx;");
+    asm("mov %r11,%rdx;");
+    asm("mov %r12,%rsi;");
+    asm("mov %r13,%rbx;");
+    asm("mov $0xb9feffffffffaaab,%rax;");
+    asm("sub %rax,%r14;");
+    asm("mov $0x1eabfffeb153ffff,%rax;");
+    asm("sbb %rax,%r15;");
+    asm("mov $0x6730d2a0f6b0f624,%rax;");
+    asm("sbb %rax,%rcx;");
+    asm("mov $0x64774b84f38512bf,%rax;");
+    asm("sbb %rax,%rdx;");
+    asm("mov $0x4b1ba7b6434bacd7,%rax;");
+    asm("sbb %rax,%rsi;");
+    asm("mov $0x1a0111ea397fe69a,%rax;");
+    asm("sbb %rax,%rbx;");
+    asm("cmovae %r14,%r8;");
+    asm("cmovae %r15,%r9;");
+    asm("cmovae %rcx,%r10;");
+    asm("cmovae %rdx,%r11;");
+    asm("cmovae %rsi,%r12;");
+    asm("cmovae %rbx,%r13;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %r12,0x20(%rdi);");
+    asm("mov %r13,0x28(%rdi);");
+    asm("pop %rbx;");
+    asm("pop %r12;");
+    asm("pop %r13;");
+    asm("pop %r14;");
+    asm("pop %r15;");
+}
+#else
 void _doubleAssign(fp* z)
 {
     uint64_t carry, _;
@@ -187,7 +428,35 @@ void _doubleAssign(fp* z)
         tie(z->d[5], _) = Sub64(z->d[5], fp::MODULUS.d[5], b);
     }
 }
+#endif
 
+#ifdef __x86_64__
+void _ldouble(fp* z, const fp* x)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // x => %rsi
+    // callee needs to restore registers r15, r14, r13, r12, rbx (if clobbed) before returning
+    asm("mov     (%rsi),%r8;");
+    asm("mov 0x08(%rsi),%r9;");
+    asm("mov 0x10(%rsi),%r10;");
+    asm("mov 0x18(%rsi),%r11;");
+    asm("mov 0x20(%rsi),%rcx;");
+    asm("mov 0x28(%rsi),%rax;");
+    asm("add %r8,%r8;");
+    asm("adc %r9,%r9;");
+    asm("adc %r10,%r10;");
+    asm("adc %r11,%r11;");
+    asm("adc %rcx,%rcx;");
+    asm("adc %rax,%rax;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %rcx,0x20(%rdi);");
+    asm("mov %rax,0x28(%rdi);");
+}
+#else
 void _ldouble(fp* z, const fp* x)
 {
     uint64_t carry, _;
@@ -199,7 +468,65 @@ void _ldouble(fp* z, const fp* x)
     tie(z->d[4], carry) = Add64(x->d[4], x->d[4], carry);
     tie(z->d[5], _)     = Add64(x->d[5], x->d[5], carry);
 }
+#endif
 
+#ifdef __x86_64__
+void _sub(fp* z, const fp* x, const fp* y)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // x => %rsi
+    // y => %rdx
+    // callee needs to restore registers r15, r14, r13, r12, rbx before returning
+    asm("push %r15;");
+    asm("push %r14;");
+    asm("push %r13;");
+    asm("push %r12;");
+    asm("push %rbx;");
+    asm("xor %rax,%rax;");
+    asm("mov     (%rsi),%r8;");
+    asm("mov 0x08(%rsi),%r9;");
+    asm("mov 0x10(%rsi),%r10;");
+    asm("mov 0x18(%rsi),%r11;");
+    asm("mov 0x20(%rsi),%r12;");
+    asm("mov 0x28(%rsi),%r13;");
+    asm("sub     (%rdx),%r8;");
+    asm("sbb 0x08(%rdx),%r9;");
+    asm("sbb 0x10(%rdx),%r10;");
+    asm("sbb 0x18(%rdx),%r11;");
+    asm("sbb 0x20(%rdx),%r12;");
+    asm("sbb 0x28(%rdx),%r13;");
+    asm("mov $0xb9feffffffffaaab,%r14;");
+    asm("mov $0x1eabfffeb153ffff,%r15;");
+    asm("mov $0x6730d2a0f6b0f624,%rcx;");
+    asm("mov $0x64774b84f38512bf,%rdx;");
+    asm("mov $0x4b1ba7b6434bacd7,%rsi;");
+    asm("mov $0x1a0111ea397fe69a,%rbx;");
+    asm("cmovae %rax,%r14;");
+    asm("cmovae %rax,%r15;");
+    asm("cmovae %rax,%rcx;");
+    asm("cmovae %rax,%rdx;");
+    asm("cmovae %rax,%rsi;");
+    asm("cmovae %rax,%rbx;");
+    asm("add %r14,%r8;");
+    asm("adc %r15,%r9;");
+    asm("adc %rcx,%r10;");
+    asm("adc %rdx,%r11;");
+    asm("adc %rsi,%r12;");
+    asm("adc %rbx,%r13;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %r12,0x20(%rdi);");
+    asm("mov %r13,0x28(%rdi);");
+    asm("pop %rbx;");
+    asm("pop %r12;");
+    asm("pop %r13;");
+    asm("pop %r14;");
+    asm("pop %r15;");
+}
+#else
 void _sub(fp* z, const fp* x, const fp* y)
 {
     uint64_t b;
@@ -220,7 +547,64 @@ void _sub(fp* z, const fp* x, const fp* y)
         tie(z->d[5], _) = Add64(z->d[5], fp::MODULUS.d[5], c);
     }
 }
+#endif
 
+#ifdef __x86_64__
+void _subAssign(fp* z, const fp* x)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // x => %rsi
+    // callee needs to restore registers r15, r14, r13, r12, rbx before returning
+    asm("push %r15;");
+    asm("push %r14;");
+    asm("push %r13;");
+    asm("push %r12;");
+    asm("push %rbx;");
+    asm("xor %rax,%rax;");
+    asm("mov     (%rdi),%r8;");
+    asm("mov 0x08(%rdi),%r9;");
+    asm("mov 0x10(%rdi),%r10;");
+    asm("mov 0x18(%rdi),%r11;");
+    asm("mov 0x20(%rdi),%r12;");
+    asm("mov 0x28(%rdi),%r13;");
+    asm("sub     (%rsi),%r8;");
+    asm("sbb 0x08(%rsi),%r9;");
+    asm("sbb 0x10(%rsi),%r10;");
+    asm("sbb 0x18(%rsi),%r11;");
+    asm("sbb 0x20(%rsi),%r12;");
+    asm("sbb 0x28(%rsi),%r13;");
+    asm("mov $0xb9feffffffffaaab,%r14;");
+    asm("mov $0x1eabfffeb153ffff,%r15;");
+    asm("mov $0x6730d2a0f6b0f624,%rcx;");
+    asm("mov $0x64774b84f38512bf,%rdx;");
+    asm("mov $0x4b1ba7b6434bacd7,%rsi;");
+    asm("mov $0x1a0111ea397fe69a,%rbx;");
+    asm("cmovae %rax,%r14;");
+    asm("cmovae %rax,%r15;");
+    asm("cmovae %rax,%rcx;");
+    asm("cmovae %rax,%rdx;");
+    asm("cmovae %rax,%rsi;");
+    asm("cmovae %rax,%rbx;");
+    asm("add %r14,%r8;");
+    asm("adc %r15,%r9;");
+    asm("adc %rcx,%r10;");
+    asm("adc %rdx,%r11;");
+    asm("adc %rsi,%r12;");
+    asm("adc %rbx,%r13;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %r12,0x20(%rdi);");
+    asm("mov %r13,0x28(%rdi);");
+    asm("pop %rbx;");
+    asm("pop %r12;");
+    asm("pop %r13;");
+    asm("pop %r14;");
+    asm("pop %r15;");
+}
+#else
 void _subAssign(fp* z, const fp* x)
 {
     uint64_t b;
@@ -241,7 +625,35 @@ void _subAssign(fp* z, const fp* x)
         tie(z->d[5], _) = Add64(z->d[5], fp::MODULUS.d[5], c);
     }
 }
+#endif
 
+#ifdef __x86_64__
+void _lsubAssign(fp* z, const fp* x)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // x => %rsi
+    // callee needs to restore registers r15, r14, r13, r12, rbx before returning
+    asm("mov     (%rdi),%r8;");
+    asm("mov 0x08(%rdi),%r9;");
+    asm("mov 0x10(%rdi),%r10;");
+    asm("mov 0x18(%rdi),%r11;");
+    asm("mov 0x20(%rdi),%rcx;");
+    asm("mov 0x28(%rdi),%rax;");
+    asm("sub     (%rsi),%r8;");
+    asm("sbb 0x08(%rsi),%r9;");
+    asm("sbb 0x10(%rsi),%r10;");
+    asm("sbb 0x18(%rsi),%r11;");
+    asm("sbb 0x20(%rsi),%rcx;");
+    asm("sbb 0x28(%rsi),%rax;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %rcx,0x20(%rdi);");
+    asm("mov %rax,0x28(%rdi);");
+}
+#else
 void _lsubAssign(fp* z, const fp* x)
 {
     uint64_t b, _;
@@ -252,12 +664,49 @@ void _lsubAssign(fp* z, const fp* x)
     tie(z->d[4], b) = Sub64(z->d[4], x->d[4], b);
     tie(z->d[5], _) = Sub64(z->d[5], x->d[5], b);
 }
+#endif
 
+#ifdef __x86_64__
+void __neg(fp* z, const fp* x)
+{
+    // x86_64 calling convention (https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI):
+    // z => %rdi
+    // x => %rsi
+    // callee needs to restore registers r15, r14, r13, r12, rbx before returning
+    asm("mov $0xb9feffffffffaaab,%r8;");
+    asm("mov $0x1eabfffeb153ffff,%r9;");
+    asm("mov $0x6730d2a0f6b0f624,%r10;");
+    asm("mov $0x64774b84f38512bf,%r11;");
+    asm("mov $0x4b1ba7b6434bacd7,%rcx;");
+    asm("mov $0x1a0111ea397fe69a,%rax;");
+    asm("sub     (%rsi),%r8;");
+    asm("sbb 0x08(%rsi),%r9;");
+    asm("sbb 0x10(%rsi),%r10;");
+    asm("sbb 0x18(%rsi),%r11;");
+    asm("sbb 0x20(%rsi),%rcx;");
+    asm("sbb 0x28(%rsi),%rax;");
+    asm("mov %r8,     (%rdi);");
+    asm("mov %r9, 0x08(%rdi);");
+    asm("mov %r10,0x10(%rdi);");
+    asm("mov %r11,0x18(%rdi);");
+    asm("mov %rcx,0x20(%rdi);");
+    asm("mov %rax,0x28(%rdi);");
+}
 void _neg(fp* z, const fp* x)
 {
     if(x->isZero())
     {
-        *z = fp::zero();
+        *z = *x;
+        return;
+    }
+    __neg(z, x);
+}
+#else
+void _neg(fp* z, const fp* x)
+{
+    if(x->isZero())
+    {
+        *z = *x;
         return;
     }
     uint64_t borrow, _;
@@ -268,6 +717,7 @@ void _neg(fp* z, const fp* x)
     tie(z->d[4], borrow) = Sub64(fp::MODULUS.d[4], x->d[4], borrow);
     tie(z->d[5], _)      = Sub64(fp::MODULUS.d[5], x->d[5], borrow);
 }
+#endif
 
 void _mul(fp* z, const fp* x, const fp* y)
 {
