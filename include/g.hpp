e36da62ddc7d885c56ba1ae15a5f55a98a19c6be
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2023-06-14 16:36:28 -0400

    add some #includes needed for building with gcc13

--- include/g.hpp
+++ include/g.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cmath>
 #include <vector>
+#include <functional>
 #include "span.h"
 
 namespace bls12_381
--- include/pairing.hpp
+++ include/pairing.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
 #include <vector>
+#include <functional>
 
 namespace bls12_381
 {
